df2e1bdc8ee907915fe508458c73f8748f87e502
Vinicius Ferreira <viniciuferreira32@gmail.com>
2021-12-19 23:50:58 -0300

    como o Filter-Button-Active

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,20 +2,27 @@ import React from 'react'
 import './Board.css'
 
 function Board() {
+
+    const handleClick = (e)=>{
+        e.target.classList.add('Active')
+        document.getElementsByClassName('Filter-Button').classList ='Filter-Button'
+    }
+
     return (
         <div className='Board'>
             <h2>Sort by</h2>
             <div className="Filter-Wraper">
-                <button className="Filter-Button">
+                <button className="Filter-Button" onClick={handleClick}>
                     <div className="Filter-Aphabetic"></div>
                 </button>
-                <button className="Filter-Button">
+                <button className="Filter-Button" onClick={handleClick}>
                     <div className="Filter-Colors"></div>
                 </button>
-                <button className="Filter-Button">
+                <button className="Filter-Button" onClick={handleClick}>
                     <div className="Filter-Sizes"></div>
                 </button>
             </div>
+
             <div className='Line'></div>
             <div className='Button'></div>
         </div>
@@ -23,5 +30,3 @@ function Board() {
 }
 
 export default Board
-
-//Espaçamento dos Filter-Button
